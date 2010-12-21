a1ef7dacde303d6975409122a96279199189785a
gumptionthomas <thomas@gumption.com>
2010-12-21 15:47:22 -0600

    Fixed bug where the shell reported being 'on_server'

--- lib/hulk/env.py
+++ lib/hulk/env.py
@@ -19,8 +19,20 @@ else:
 
 # Are we running on a google server?
 server_software = os.environ.get('SERVER_SOFTWARE', '')
-on_server = have_appserver and appid and not server_software.lower().startswith('devel')
+on_server = have_appserver and appid and server_software and not server_software.lower().startswith('devel')
+if on_server:
+    on_server = True
+else:
+    on_server = False
 # Are we running on an integration environment?
 on_integration_server = on_server and appid.lower().endswith('-int')
 # Are we running on a production environment?
 on_production_server = on_server and not on_integration_server
+
+import logging
+logging.info("have_appserver: %s" % have_appserver)
+logging.info("appid: %s" % appid)
+logging.info("server_software: %s" % server_software)
+logging.info("on_server: %s" % on_server)
+logging.info("on_integration_server: %s" % on_integration_server)
+logging.info("on_production_server: %s" % on_production_server)
