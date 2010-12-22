322a248536fe346c23c8eeb6d1568b19b3c20a89
Brian Michelich <brian@michelich.com>
2010-12-22 09:05:48 -0600

    added optional django config to ae_config

--- appengine_config.py
+++ appengine_config.py
@@ -1,1 +1,13 @@
 hulk_url_APPLICATIONS = ['main', 'api']
+
+
+
+# If your application uses Django and requires a specific version of
+# Django, uncomment the following block of three lines.  Currently
+# supported values for the Django version are '0.96' (the default),
+# '1.0', and '1.1'.
+
+#from google.appengine.dist import use_library
+#use_library('django', '1.0')
+#import django
+
