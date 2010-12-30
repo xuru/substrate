c77ebdee60ee6cad27433a421956dbdd1aa1fa66
Brian Michelich <brian@michelich.com>
2010-12-30 09:20:17 -0600

    added django 1.1 for templating

--- setup.py
+++ setup.py
@@ -3,3 +3,8 @@ import sys, os
 
 def setup():
     sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+    
+    from hulk import env
+    if env.on_development_server:
+        sys.path.append(os.path.join(os.path.dirname(__file__), "local/ext"))
+
