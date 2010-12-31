5983adf90d7fe2d7c55f850fadfc5c2c171fe48b
Brian Michelich <brian@michelich.com>
2010-12-31 11:50:28 -0600

    added session key

--- appengine_config.py
+++ appengine_config.py
@@ -51,7 +51,7 @@ hulk_url_APPLICATIONS = ['main', 'api']
 # import os
 # os.urandom(64)
 from gaesessions import SessionMiddleware
-SESSION_KEY='Replace with key'
+SESSION_KEY="\xa9\x9bl\x99&\x19\xfe\x8d\xad\xc5\xa9\xef\x7f\xde4\x1a0\xa8nu\x9f\x0f\x81\x8e\xf1D\xde\x1bA\xfc\xf7io\xc8\xbb[\xe8W\xf7\xc3@\xf0\x06\xd8\x11!\x10\xb6\x93%\x03p\xd7\xa4\x8b'(vs\x82\x93\x93e\x06"
 def webapp_add_wsgi_middleware(app):
     app = SessionMiddleware(app, cookie_key=SESSION_KEY)
     return app
