c97b64af7a0462b28917637462524821e1ccbf9d
Brian Michelich <brian@michelich.com>
2011-02-20 23:30:37 -0600

    check for lib in sys.path before adding

--- env_setup.py
+++ env_setup.py
@@ -2,9 +2,10 @@ import sys, os
 
 
 def setup():
-    sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+    if 'lib' not in sys.path:
+        sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 
-    from hulk import env
-    if env.on_development_server:
-        sys.path.append(os.path.join(os.path.dirname(__file__), "local/ext"))
+    # from hulk import env
+    # if env.on_development_server:
+    #     sys.path.append(os.path.join(os.path.dirname(__file__), "local/ext"))
 
