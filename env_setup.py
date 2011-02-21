dbdc630e059ddac8c12cdefbd3c8635f0213bf94
Brian Michelich <brian@michelich.com>
2011-02-21 12:47:33 -0600

    better fix for env_setup

--- env_setup.py
+++ env_setup.py
@@ -1,9 +1,8 @@
-import sys, os
-
+import sys
 
 def setup():
     if 'lib' not in sys.path:
-        sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+        sys.path[0:0] = ['lib']
 
     # from hulk import env
     # if env.on_development_server:
