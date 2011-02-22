3593479d1317a31eac103e5e93f9b6c95b4b3b3b
Brian Michelich <brian@michelich.com>
2011-02-22 17:11:25 -0600

    more robust setup()

--- env_setup.py
+++ env_setup.py
@@ -1,10 +1,25 @@
+import os
 import sys
 
 def setup():
-    if 'lib' not in sys.path:
-        sys.path[0:0] = ['lib']
+    '''
+    Adds <project_root>/lib to the python path.  
+    Starts in current working directory and traverses up until found.
+    Assumes app.yaml is in project root.
+    '''
+    curdir = os.path.abspath('.')
+    paths_to_search = os.path.abspath('.').split(os.path.sep)
 
-    # from hulk import env
-    # if env.on_development_server:
-    #     sys.path.append(os.path.join(os.path.dirname(__file__), "local/ext"))
+    for path in paths_to_search:
+        if os.path.exists('app.yaml'):
+            lib_path = os.path.join(os.path.abspath('.'), 'lib')
 
+            if lib_path not in sys.path:
+                sys.path.insert(0, lib_path)
+            
+            os.chdir(curdir)
+            break
+        
+        os.chdir('..')
+    else:
+        raise Exception('app.yaml not found for env_setup.setup()')
