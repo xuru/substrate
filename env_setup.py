595d5a74bd50a3ac130a3e9149ff8bee799ee4d7
Brian Michelich <brian@michelich.com>
2011-02-23 09:37:40 -0600

    more env_setup.setup changes

--- env_setup.py
+++ env_setup.py
@@ -6,20 +6,21 @@ def setup():
     Adds <project_root>/lib to the python path.  
     Starts in current working directory and traverses up until found.
     Assumes app.yaml is in project root.
-    '''
-    curdir = os.path.abspath('.')
-    paths_to_search = os.path.abspath('.').split(os.path.sep)
-
-    for path in paths_to_search:
-        if os.path.exists('app.yaml'):
-            lib_path = os.path.join(os.path.abspath('.'), 'lib')
+    '''  
+    start_path = os.path.abspath('.')
+    search_path = start_path
+    while search_path:
+        app_yaml_path = os.path.join(search_path, 'app.yaml')
+        
+        if os.path.exists(app_yaml_path):
+            lib_path = os.path.join(search_path, 'lib')
 
             if lib_path not in sys.path:
                 sys.path.insert(0, lib_path)
             
-            os.chdir(curdir)
             break
-        
-        os.chdir('..')
+        search_path, sep, last_dir = search_path.rpartition(os.path.sep)
     else:
-        raise Exception('app.yaml not found for env_setup.setup()')
+        if sys.platform == 'darwin':
+            os.system('say error app.yaml not found')
+        raise os.error('app.yaml not found for env_setup.setup().%sSearch started in: %s'% (os.linesep, start_path))
