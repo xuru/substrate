21da5d5873960bfb48e444d7089be6c502b30f0a
gumptionthomas <thomas@gumption.com>
2011-05-06 13:28:54 -0500

    Added docs and tests for agar.config

--- lib/agar/config.py
+++ lib/agar/config.py
@@ -1,6 +1,39 @@
 from google.appengine.api import lib_config
 
 class Config(object):
+    """
+    Configurable constants using 'lib_config' wrapper.
+
+    To use this class, create a subclass that redefines '_namespace' to the appengine_config namespace you'd like the
+    configs to appear under.  Then, simply create class-level properties/default values for each constant.
+
+    When instantiating an instance of this class, you can override the default values for that instance by passing
+    in new defaults via the constructor.  Of course, if there is an entry in appengine_config for your constant, that
+    value will supersede any defined in the class or passed in via the constructor.
+
+    Example:
+
+    class SampleConfig(Config):
+        _namespace = 'test'
+
+        STRING_CONFIG = 'defaultstring'
+
+    >>> config = SampleConfig.get_config()
+    >>> custom_config = SampleConfig.get_config(STRING_CONFIG='customstring')
+
+    #Assuming no override for 'test_STRING_CONFIG' in appengine_config.py:
+    >>> config.STRING_CONFIG == 'defaultstring'
+    True
+    >>> custom_config.STRING_CONFIG == 'customstring'
+    True
+
+    #Assuming appengine_config.py contains the following line:
+    test_STRING_CONFIG = 'settingstring'
+
+    #Then:
+    >>> config.STRING_CONFIG == custom_config.STRING_CONFIG == 'settingstring'
+    True
+    """
     _namespace = 'agar'
 
     def __init__(self, **kwargs):
