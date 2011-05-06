f3f1dfb83e13fbef4fc2e10d282fce7a8ef5551b
gumptionthomas <thomas@gumption.com>
2011-05-06 13:28:54 -0500

    Added docs and tests for agar.config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,25 @@
 import unittest
 
+from agar.config import Config
+
+class SampleConfig(Config):
+    _namespace = 'test'
+
+    STRING_CONFIG = 'defaultstring'
+
 
 class ConfigTest(unittest.TestCase):
     def test_config(self):
-        from google.appengine.api import lib_config
-        config = lib_config.register('agar_url', {'APPLICATIONS': []})
-        self.assertEqual(config.APPLICATIONS, ['main', 'api'])
+        config = SampleConfig.get_config()
+        self.assertEqual(config.STRING_CONFIG, 'defaultstring')
+
+    def test_override_config(self):
+        config = SampleConfig.get_config(STRING_CONFIG='customstring')
+        self.assertEqual(config.STRING_CONFIG, 'customstring')
+
+    def test_invalid_override(self):
+        try:
+            SampleConfig.get_config(INVALID_CONFIG=None)
+            self.fail("Able to set an invalid config property")
+        except AttributeError, e:
+            self.assertEqual(e.message, "Invalid config key: INVALID_CONFIG")
