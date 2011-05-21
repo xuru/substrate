1f1bc1f8e628e7a9013b24ef7d8fac176243db77
Brian Michelich <brian@michelich.com>
2011-05-20 19:27:54 -0500

    more NamedModel tests

--- tests/test_named_model.py
+++ tests/test_named_model.py
@@ -21,13 +21,35 @@ class NamedModelTests(DataStoreTestCase, TestCase):
         self.assertIsNone(model.key().id())
         self.assertIsNotNone(model.key().name())
     
-    def test_create_new_entity_with_key_name(self):
+    def test_create_with_key_name(self):
         class TestModel(NamedModel):
             string = db.StringProperty(required=True)
         
         model = TestModel.create_new_entity(string='test entity', key_name='test_key')
         self.assertIsNone(model.key().id())
         self.assertEquals('test_key', model.key().name())
+    
+    def test_create_with_duplicate_key_name(self):
+        class TestModel(NamedModel):
+            string = db.StringProperty(required=True)
+        
+        model1 = TestModel.create_new_entity(string='test entity1', key_name='test_key')
+        model2 = TestModel.create_new_entity(string='test entity2', key_name='test_key')
+        self.assertIsNone(model2)
+
+    def test_create_with_key_name_and_parent(self):
+        class ParentModel(NamedModel):
+            string = db.StringProperty(required=True)
+
+        class TestModel(NamedModel):
+            string = db.StringProperty(required=True)
+        
+        parent = ParentModel.create_new_entity(string='parent')
+        model = TestModel.create_new_entity(
+            string='test entity', key_name='test_key', parent=parent.key())
+        self.assertIsNone(model.key().id())
+        self.assertEquals('test_key', model.key().name())
+        self.assertEquals(parent.key(), model.parent_key())
 
     def test_key_name_property(self):
         class TestModel(NamedModel):
