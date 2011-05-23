84e7dbe15af81169c186666daf3c32fb8d4a98ff
Brian Michelich <brian@michelich.com>
2011-05-23 12:18:48 -0500

    fixed sphinx settings.py import issue

--- docs/static/conf.py
+++ docs/static/conf.py
@@ -18,6 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('./../../lib'))
+sys.path.insert(0, os.path.abspath('./../../../substrate'))
 sys.path.insert(0, os.path.abspath('/usr/local/google_appengine'))
 
 from google.appengine.dist import use_library
