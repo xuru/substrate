58dfde29500728829a921bffcfd8fb3d3d5b04b5
Curtis Thompson <curtis@bitbucket.org>
2011-06-22 16:26:55 -0500

    Fix bad merge

--- local/commands/test.py
+++ local/commands/test.py
@@ -50,14 +50,10 @@ import sys
 # main(plugins=[NoseGAE(), NoseExclude(), LogCapture()])
 if __name__ == "__main__":
     if len (sys.argv) < 2:
-    print "No tests specified.  Running everything..."
+        print "No tests specified.  Running everything..."
         sys.argv[0] = "."
-else:
+    else:
         print "Testing: %s"% sys.argv[1]
         sys.argv[0] = sys.argv[1]
         del sys.argv[1]
-
-# Would like to get the log capture working sometime...
-# main(plugins=[NoseGAE(), NoseExclude(), LogCapture()])
-main(plugins=[NoseGAE(), NoseExclude()])
 
