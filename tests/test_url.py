5883d0ff060607a3c621c39d61d49b0f46f2a6a9
gumptionthomas <thomas@gumption.com>
2011-07-26 14:33:20 -0500

    Clean up docs

--- tests/test_url.py
+++ tests/test_url.py
@@ -9,10 +9,11 @@ from agar.url import uri_for, url_for
 
 class UriTest(WebTestCase, TestCase):
     def setUp(self):
+        self.method = uri_for
         super(WebTestCase, self).setUp()
 
     def test_get_uri(self):
-        uri = uri_for('api-v1')
+        uri = self.method('api-v1')
         self.assertEqual(uri, '/api/v1/model1')
 
     def test_get_invalid_uri_name(self):
@@ -23,18 +24,7 @@ class UriTest(WebTestCase, TestCase):
         except Exception, e:
             self.assertEqual(e.message, "Route named '%s' is not defined." % invalid_uri_name)
 
-class UrlTest(WebTestCase, TestCase):
+class UrlTest(UriTest):
     def setUp(self):
+        self.method = url_for
         super(WebTestCase, self).setUp()
-
-    def test_get_url(self):
-        url = url_for('api-v1')
-        self.assertEqual(url, '/api/v1/model1')
-
-    def test_get_invalid_url_name(self):
-        try:
-            invalid_url_name = 'invalid-url-name'
-            url = url_for(invalid_url_name)
-            self.fail("Got uri '%s' for invalid url name '%s'" % (url, invalid_url_name))
-        except Exception, e:
-            self.assertEqual(e.message, "Route named '%s' is not defined." % invalid_url_name)
