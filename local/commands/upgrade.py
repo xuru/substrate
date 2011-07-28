034afc578d63ceec9b3a2948814a27627db737de
Brian Michelich <brian@michelich.com>
2011-07-27 21:20:13 -0500

    brought over changes from argparse branch

--- local/commands/upgrade.py
+++ local/commands/upgrade.py
@@ -1,3 +1,5 @@
+""" Pulls the latest version of substrate to ~/.substrate and replaces the project files."""
+
 import os
 import shutil
 
@@ -7,32 +9,34 @@ import shutil
 # TODO make substrate repo public
 # TODO check to see if project to be upgraded has no uncommitted files
 
-upgrade_items = ['manage.py', 'env_setup.py', 'local/commands', 'local/lib', 'lib/agar']
+
+if __name__ == '__main__':
+    upgrade_items = ['manage.py', 'env_setup.py', 'local/commands', 'local/lib', 'lib/agar']
 
 
-current_dir = os.path.abspath('.')
-substrate_home_dir = os.path.expanduser('~/.substrate')
-substrate_repo = os.path.expanduser('%s/substrate' % substrate_home_dir)
+    current_dir = os.path.abspath('.')
+    substrate_home_dir = os.path.expanduser('~/.substrate')
+    substrate_repo = os.path.expanduser('%s/substrate' % substrate_home_dir)
 
 
-if not os.path.isdir(substrate_home_dir):
-    os.mkdir(os.path.expanduser(substrate_home_dir))
+    if not os.path.isdir(substrate_home_dir):
+        os.mkdir(os.path.expanduser(substrate_home_dir))
 
 
-if os.path.isdir(substrate_repo):
-    os.chdir(substrate_repo)
-    os.system('hg fetch')
-    os.chdir(current_dir)
-else:
-    os.system('hg clone ssh://hg@bitbucket.org/garykoelling/substrate %s' % substrate_repo)
+    if os.path.isdir(substrate_repo):
+        os.chdir(substrate_repo)
+        os.system('hg fetch')
+        os.chdir(current_dir)
+    else:
+        os.system('hg clone ssh://hg@bitbucket.org/garykoelling/substrate %s' % substrate_repo)
 
 
-for item in upgrade_items:
-    item_path = '%s/%s' % (substrate_repo, item)
+    for item in upgrade_items:
+        item_path = '%s/%s' % (substrate_repo, item)
 
-    if os.path.isfile(item_path):
-        shutil.copy(item_path, current_dir)
-    
-    if os.path.isdir(item_path):
-        shutil.rmtree('%s/%s' % (current_dir, item))
-        shutil.copytree(item_path, '%s/%s' % (current_dir, item))
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, current_dir)
+        
+        if os.path.isdir(item_path):
+            shutil.rmtree('%s/%s' % (current_dir, item))
+            shutil.copytree(item_path, '%s/%s' % (current_dir, item))
