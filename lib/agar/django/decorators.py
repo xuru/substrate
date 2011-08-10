a0551c2c3341a57e6c366943d83356d54571abdc
Sean M. Duncan <bitbutcher@gmail.com>
2011-08-10 16:31:20 -0500

    added configurable form validation error logging to agar.django.decorators

--- lib/agar/django/decorators.py
+++ lib/agar/django/decorators.py
@@ -2,7 +2,9 @@
 The ``agar.django.decorators`` module contains functions and decorators to help validate `django forms`_,
 to be used to wrap :py:class:`agar.json.JsonRequestHandler` methods that accept input.
 """
+import logging
 
+from agar.django import config
 
 def create_error_dict(error_list):
     from django.forms.util import ErrorList
@@ -14,7 +16,7 @@ def create_error_dict(error_list):
             text_errors[key] = value
     return text_errors
 
-def validate_service(form_class):
+def validate_service(form_class, log_errors=config.LOG_SERVICE_VALIDATION_ERRORS, log_values=config.LOG_SERVICE_VALIDATION_VALUES):
     """
     A decorator that validates input matches with a `django form class`_.
 
@@ -25,18 +27,39 @@ def validate_service(form_class):
     error dictionary describing the input errors.
 
     :param form_class: The `django form class`_ to use for input validation.
+    :param log_errors: The logging level for form validation errors.  Defaults to `agar_django_LOG_SERVICE_VALIDATION_ERRORS` (off).
+    :param log_values: `True` if the raw request parameter values should be logged for fields with validation errors. Defaults to 
+        `agar_django_LOG_SERVICE_VALIDATION_VALUE`.
     """
+
+    def log(handler, error_dict):
+        if log_errors:
+            log_template = 'Invalid api call: %(errors)s'
+            if hasattr(handler.request, 'account'):
+                log_template = 'Invalid api call from "%(account)s": %(errors)s'
+            error_template = 'field "%(field)s" has error "%(error)s"'
+            if log_values:
+                error_template += ' for value "%(value)s"'
+            logging.log(log_errors, log_template % { 
+                'errors': ' and '.join([ 
+                    error_template % {'field': key, 'error': value, 'value': handler.request.params.get(key)} 
+                    for key, value in error_dict.items()
+                ]),
+                'account': str(getattr(handler.request, 'account', None))
+            })
+
     def decorator(request_method):
-        def wrapped(self, *args, **kwargs):
-            form = form_class(self.request.params)
+        def wrapped(handler, *args, **kwargs):
+            form = form_class(handler.request.params)
             if form.is_valid():
-                self.request.form = form
-                request_method(self, *args, **kwargs)
+                handler.request.form = form
+                request_method(handler, *args, **kwargs)
                 return
             else:
                 error_dict = create_error_dict(form.errors)
+                log(handler, error_dict)
                 status_text = "BAD_REQUEST"
-                self.json_response({}, status_code=400, status_text=status_text, errors=error_dict)
+                handler.json_response({}, status_code=400, status_text=status_text, errors=error_dict)
                 return
         return wrapped
     return decorator
