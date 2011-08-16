cb509e4737f30c6239ba62f5705c24ff50eeb37c
gumptionthomas <thomas@gumption.com>
2011-08-16 14:27:42 -0500

    Removed gaesessions in favor of webapp2 sessions supported by agar.sessions

--- appengine_config.py
+++ appengine_config.py
@@ -19,19 +19,3 @@ agar_url_APPLICATIONS = ['main', 'api']
 
 # Enable logging of raw request value of fields with validation errors for the validate_service decorator
 # agar_django_LOG_SERVICE_VALIDATION_VALUES = True
-
-##############################################################################
-# SESSION SETTINGS
-##############################################################################
-
-# create a SESSION_KEY
-# import os
-# os.urandom(64)
-
-#SESSION_KEY="\xa9\x9bl\x99&\x19\xfe\x8d\xad\xc5\xa9\xef\x7f\xde4\x1a0\xa8nu\x9f\x0f\x81\x8e\xf1D\xde\x1bA\xfc\xf7io\xc8\xbb[\xe8W\xf7\xc3@\xf0\x06\xd8\x11!\x10\xb6\x93%\x03p\xd7\xa4\x8b'(vs\x82\x93\x93e\x06"
-#def webapp_add_wsgi_middleware(app):
-#    from gaesessions import SessionMiddleware
-#    app = SessionMiddleware(app, cookie_key=SESSION_KEY)
-#    return app
-#
-#
