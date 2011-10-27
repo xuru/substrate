ffcf81dd7d005ceeedb570fbb7f1b92527b3b665
gumptionthomas <thomas@gumption.com>
2011-10-27 14:34:46 -0500

    Updated version to 0.3d. Updated agar to latest 0.3d version. Updated docs and CHANGES.

--- app/lib/agar/test/mock_urlfetch_test.py
+++ app/lib/agar/test/mock_urlfetch_test.py
@@ -44,6 +44,35 @@ class MockURLFetchServiceStub(apiproxy_stub.APIProxyStub):
 
 
 class MockUrlfetchTest(BaseTest):
+    """
+    :py:class:`MockUrlfetchTest` replaces the `urlfetch`_ API stub with a mocked
+    version that does not make real HTTP requests.
+
+    To use it, inherit from :py:class:`MockUrlfetchTest` instead of
+    :py:class:`BaseTest`, then register HTTP responses in your
+    ``setUp`` method, or individual test case methods.
+
+    If any of the code under test results in `urlfetch`_ call to an
+    unregistered URL, it will raise an exception.
+
+    Example::
+
+       class MyHTTPTest(MockUrlfetchTest):
+           def setUp(self):
+               super(MyHTTPTest, self).setUp()
+
+               self.set_response("http://www.google.com/blah", "foobar", 404)
+
+           def test_get_google(self):
+               result = urlfetch.fetch("http://www.google.com/blah")
+
+               self.assertEqual(404, result.status_code)
+               self.assertEqual("foobar", result.content)
+
+           def test_this_will_fail(self):
+               result = urlfetch.fetch("http://www.example.com/")
+    """
+
     def setUp(self):
         super(MockUrlfetchTest, self).setUp()
 
@@ -55,4 +84,12 @@ class MockUrlfetchTest(BaseTest):
         super(MockUrlfetchTest, self).tearDown()
         
     def set_response(self, url, content, status_code=None, headers=None):
+        """
+        Register an HTTP response for ``url`` with body containing ``content``.
+
+        :param url: the URL for the HTTP request
+        :param content: the HTTP response's body
+        :param status_code: the expected status code. Defaults to 200 if not set.
+        :param headers: a ``dict`` of headers for the HTTP response.
+        """
         MockURLFetchServiceStub.set_response(url, content, status_code, headers)
