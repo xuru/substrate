bd20624ca516ab124df00b6957f974bb76b7f7d0
Brian Michelich <brian@michelich.com>
2011-12-31 17:28:38 -0600

    removed use_libary, use app.yaml.  pep8 formatting

--- app/env_setup.py
+++ app/env_setup.py
@@ -2,13 +2,15 @@
 Functions to initialize environment settings.
 """
 
+
 def setup():
-    """Adds <project_root>/lib to the python path.  
-    
+    """Adds <project_root>/lib to the python path.
+
     Starts in current working directory and traverses up until app.yaml is found.
     Assumes app.yaml is in project root.
     """
-    import os, sys
+    import os
+    import sys
     start_path = os.path.abspath('.')
     search_path = start_path
     while search_path:
@@ -23,20 +25,16 @@ def setup():
             break
         search_path, last_dir = os.path.split(search_path)
     else:
-        raise os.error('app.yaml not found for env_setup.setup().%sSearch started in: %s'% (os.linesep, start_path))
+        raise os.error('app.yaml not found for env_setup.setup().%sSearch started in: %s' % (os.linesep, start_path))
 
 
-#  TODO remove and test against py2.7
-def setup_django(settings='settings', version='1.2', ):
+def setup_django(settings='settings'):
     """
-    Sets up the django libraries.
+    Sets up the django settings.
 
     :param settings: The name of the settings file. Default: ``'settings'``.
-    :param version: The django version to set up. Default: ``'1.2'``.
     """
     import os
     os.environ['DJANGO_SETTINGS_MODULE'] = settings
-    from google.appengine.dist import use_library
-    use_library('django', version)
     from django.conf import settings
     _ = settings.TEMPLATE_DIRS
