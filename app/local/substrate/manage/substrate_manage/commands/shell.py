634e5409587d2e7820c59e7d7ee7a7904bc1af27
gumptionthomas <thomas@gumption.com>
2012-01-30 12:43:09 -0600

    Updated manage.py shell command to accept settings for the database paths

--- app/local/substrate/manage/substrate_manage/commands/shell.py
+++ app/local/substrate/manage/substrate_manage/commands/shell.py
@@ -1,6 +1,8 @@
 """ Run an interactive console after including AppEngine and project libraries. """
 
+import getopt
 import logging
+import sys
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,14 +18,41 @@ try:
     config, matcher, from_cache = dev_appserver.LoadAppConfig(".", {}, default_partition='dev')
 except yaml_errors.EventListenerError, e:
     logging.error('Fatal error when loading application configuration:\n' +
-                                    str(e))
+                  str(e))
 except dev_appserver.InvalidAppConfigError, e:
     logging.error('Application configuration file invalid:\n%s', e)
 
-args = dev_appserver_main.DEFAULT_ARGS.copy()
-dev_appserver.SetupStubs(config.application, **args)
+appserver_args = dev_appserver_main.DEFAULT_ARGS.copy()
+
+def usage():
+    print """%s
+
+Usage: python manage.py shell
+
+Options:
+  -b DIR, --blobstore_path=DIR          Path to directory to use for storing Blobstore file stub data.
+  -d DS_FILE, --datastore_path=DS_FILE  Path to file to use for storing Datastore file stub data.
+                                        (Default %s)
+  -h, --help                            Show this help.
+""" % (__doc__, appserver_args[dev_appserver_main.ARG_DATASTORE_PATH])
 
 if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hd:b:", ["help", "datastore_path=", "blobstore_path="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-d", "--datastore_path"):
+            appserver_args[dev_appserver_main.ARG_DATASTORE_PATH] = arg
+        elif opt in ("-b", "--blobstore_path"):
+            appserver_args[dev_appserver_main.ARG_BLOBSTORE_PATH] = arg
+
+    dev_appserver.SetupStubs(config.application, **appserver_args)
+
     banner = "Interactive App Engine Shell for app-id '%s'" % config.application
     try:
         import IPython
