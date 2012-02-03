1dfab00acb300943d23dccefe53f4cd8bc8c2daf
gumptionthomas <thomas@gumption.com>
2012-02-02 22:35:58 -0600

    Fix python 2.7 sample app tests

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -2,9 +2,7 @@
 The configuration file used by :py:mod:`agar.config` implementations and other libraries using the
 `google.appengine.api.lib_config`_ configuration library. Configuration overrides go in this file.
 """
-
-from env_setup import setup
-setup()
+from env_setup import setup; setup()
 
 ##############################################################################
 # AGAR SETTINGS
