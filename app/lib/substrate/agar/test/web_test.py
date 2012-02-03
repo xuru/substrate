4c78e87df3da51d7acf517cd6a08664ce7e72ff4
gumptionthomas <thomas@gumption.com>
2012-02-02 22:59:07 -0600

    Remove unittest2 in favor of python2.7 unit test

--- app/lib/substrate/agar/test/web_test.py
+++ app/lib/substrate/agar/test/web_test.py
@@ -1,7 +1,11 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 import webtest
-from unittest2 import TestCase
 
-class WebTest(TestCase):
+
+class WebTest(unittest.TestCase):
     """
     A base class for testing web requests. Provides a wrapper around
     the `WebTest`_ package that is mostly compatable with `gaetestbed`_'s.
