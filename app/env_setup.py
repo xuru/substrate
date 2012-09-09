9ecde0cc5136cb94500a1093eec4159bb467f124
Brian Michelich <brian@michelich.com>
2012-09-08 19:17:55 -0500

    fixed django version warning for example test

--- app/env_setup.py
+++ app/env_setup.py
@@ -36,14 +36,17 @@ def setup():
             sys.path.insert(0, lib_usr_path)
 
 
-def setup_django(settings='settings'):
+def setup_django(settings='settings', version='1.3', ):
     """
-    Sets up the django settings.
+    Sets up the django libraries.
 
-    :param settings: The name of the settings file. Default-- ``'settings'``.
+    :param settings: The name of the settings file. Default: ``'settings'``.
+    :param version: The django version to set up. Default: ``'1.3'``.
     """
     import os
     os.environ['DJANGO_SETTINGS_MODULE'] = settings
+    from google.appengine.dist import use_library
+    use_library('django', version)
     from django.conf import settings
     _ = settings.TEMPLATE_DIRS
 
